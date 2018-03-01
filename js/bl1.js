9e861de8d8e135329cb9ec4036462a8c6b7c9653
Melinda Tofalvi <36535605+MelindaTofalvi@users.noreply.github.com>
2018-03-01 16:00:05 +0000

    Add files via upload

--- js/bl1.js
+++ js/bl1.js
@@ -15,52 +15,52 @@ GAME
 =======================
 */
 
-
-  $( "#dialog" ).dialog({
+// dont't auto open the dialog box
+$( "#dialog" ).dialog({
     autoOpen: false
 });
 
+//cheat
+$(document).keypress(function(event){
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+	if(keycode == '13'){
+		callDialog();
+	}
+});
 
+// variables for the game
 var BoxOpened = "";
 var ImgOpened = "";
 var Counter = 0;
 var ImgFound = 0;
 var gameOver = false;
-
 var Source = "#boxcard";
 
+//images for the game
 var ImgSource = [
   "img/level1/hp1.jpg",
   "img/level1/hp2.jpg",
   "img/level1/hp3.jpg",
-  "img/level1/hp4.jpg",
-  "img/level1/hp5.jpg"
+  "img/level1/hp4.jpg"
 ];
 
-$(document).keypress(function(event){
-
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if(keycode == '13'){
-		callDialog();
-	}
-
-});
-
+//creating card holder divs
 $(function() {
 	for (var y = 1; y < 3 ; y++) {
 		$.each(ImgSource, function(i, val) {
 			$(Source).append("<div id=card" + y + i + "><img src=" + val + " />");
 		});
 	}
-		$(Source + " div").click(OpenCard);
-		ShuffleImages();
+	$(Source + " div").click(OpenCard);
+	ShuffleImages();
 });
 
-
+// get random number for Shuffleimages() function
 function RandomFunction(MaxValue, MinValue) {
-		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
-	}
-	
+	return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+}
+
+//shuffle images
 function ShuffleImages() {
 	var ImgAll = $(Source).children();
 	var ImgThis = $(Source + " div:first-child");
@@ -70,12 +70,9 @@ function ShuffleImages() {
 		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
 		ImgThis = ImgThis.next();
 	}
-	
-		ImgThis = $(Source + " div:first-child");
-	
+	ImgThis = $(Source + " div:first-child");
 	for (var z = 0; z < ImgAll.length; z++) {
-	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
-
+		var RandomNumber = RandomFunction(0, ImgArr.length - 1);
 		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
 		ImgArr.splice(RandomNumber, 1);
 		ImgThis = ImgThis.next();
@@ -95,58 +92,45 @@ $("#picbox").one("click", function(e){
 			$(".timer span").text(sec + " sec");
 		}
 		if(timerOn === false){
-		clearInterval(timer);
-	}
-		
+			clearInterval(timer);
+		}
 	}, 1000);
-	
-	})
+})
 
-
+// open card on click
 function OpenCard() {
-
 	var id = $(this).attr("id");
-
 	if ($("#" + id + " img").is(":hidden")) {
 		$(Source + " div").unbind("click", OpenCard);
-		
-		
-
 		$("#" + id + " img").fadeIn('slow');
-
-
 		if (ImgOpened == "") {
 			$("#" + id + " img").css("box-shadow", "").css("transition", "");
-			
 			BoxOpened = id;
 			ImgOpened = $("#" + id + " img").attr("src");
 			setTimeout(function() {
-				$(Source + " div").bind("click", OpenCard)
+				$(Source + " div").bind("click", OpenCard);
 			}, 300);
+	} else {
+		CurrentOpened = $("#" + id + " img").attr("src");
+		$("#" + id + " img").css("box-shadow", "").css("transition", "");
+		$("#" + BoxOpened + " img").css("box-shadow", "").css("transition", "");
+		if (ImgOpened != CurrentOpened) {
+			setTimeout(function() {
+				$("#" + id + " img").css("box-shadow", "0 0 1em 0.5em maroon").css("transition", "outline 0.6s linear");
+				$("#" + BoxOpened + " img").css("box-shadow", "0 0 1em 0.5em maroon").css("transition", "outline 0.6s linear");
+				setTimeout(function() {		
+					$("#" + id + " img").fadeOut(500);
+					$("#" + BoxOpened + " img").fadeOut(500);
+					BoxOpened = "";
+					ImgOpened = "";		
+				}, 500);	
+			}, 1000);
 		} else {
-			CurrentOpened = $("#" + id + " img").attr("src");
-			$("#" + id + " img").css("box-shadow", "").css("transition", "");
-			$("#" + BoxOpened + " img").css("box-shadow", "").css("transition", "");
-			if (ImgOpened != CurrentOpened) {
-				setTimeout(function() {
-					$("#" + id + " img").css("box-shadow", "0 0 1em 0.5em maroon").css("transition", "outline 0.6s linear");
-					$("#" + BoxOpened + " img").css("box-shadow", "0 0 1em 0.5em maroon").css("transition", "outline 0.6s linear");
-					setTimeout(function() {
-						$("#" + id + " img").fadeOut(500);
-						$("#" + BoxOpened + " img").fadeOut(500);
-						BoxOpened = "";
-						ImgOpened = "";
-					}, 1000);
-					
-					
-				}, 1000);
-			} else {
-				$("#" + id + " img").css("box-shadow", "0 0 1em 0.5em green").css("transition", "outline 0.6s linear");
-				$("#" + BoxOpened + " img").css("box-shadow", "0 0 1em 0.5em green").css("transition", "outline 0.6s linear");
-				setTimeout(function() {
-					$("#" + id + " img").parent().css("visibility", "hidden");
+			$("#" + id + " img").css("box-shadow", "0 0 1em 0.5em green").css("transition", "outline 0.6s linear");
+			$("#" + BoxOpened + " img").css("box-shadow", "0 0 1em 0.5em green").css("transition", "outline 0.6s linear");
+			setTimeout(function() {
+				$("#" + id + " img").parent().css("visibility", "hidden");
 				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
-				
 				BoxOpened = "";
 				ImgOpened = "";
 				}, 1000);
@@ -154,23 +138,21 @@ function OpenCard() {
 			}
 			setTimeout(function() {
 				$(Source + " div").bind("click", OpenCard)
-			}, 400);
+			}, 1501);	
 		}
-		console.log(ImgFound.length);
 		Counter++;
 		$("#counter").html("" + Counter);
 		if (ImgFound == ImgSource.length) {
-			console.log("hello");
 			$("#counter").prepend('<span id="success">You Found All Pictues With </span>');
 			timerOn = false;
 			setTimeout(function() {
 				callDialog();
 			}, 1500);
-			
 		}
 	}
 }
 
+// game over dialog
 function callDialog(){
 	$('#dialog-confirm')
   // Dialog will slide down from top of document
@@ -178,8 +160,8 @@ function callDialog(){
     var widget = $(this).dialog('widget'),
         offset = widget.offset();
     widget
-      .css('top', -widget.outerWidth() + 'px')
-      .animate({ top: offset.top }, { duration: 300 });
+    .css('top', -widget.outerWidth() + 'px')
+    .animate({ top: offset.top }, { duration: 1000 });
   })
     .dialog({
     resizable: false,
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Harry Potter Memory Game
+
+A memory card game with three levels. Theme is Harry Potter.
+
+##Purpose
+A fully functional web based game developed with Javascript, jQuery, HTML, CSS and Bootstrap.
+
+##Functionality
+
+Markup has been styled to be responsive, according to the device used to view the app.
+
+DRY, well-commented code, properly indented.
+
+##Content
+* Starting page
+* 3 levels
+* Animation page
+
+![Screen shot of level 1](img/screenShot.png)
+
+##How to play
+On the starting page select "Play" to start the game.
+Choose a card and turn it over by clicking on it. Then select another card and turn it over.
+If the two cards are a matching pair then they disappear. If not, they both turn back. Then you chose another card. The process continues until all cards on the board will be turned over.
+After winning the first game, you will be directed to the next page where Harry gets one step closer to Hogwarts. Then you have to play two more rounds of memory game. Each winnings will move Harry further in his goal to reach Hogwarts. Getting to Hogwarts would mean the beginning of another long-waited year at Hogwarts which he calls home.
+
+##Implementation
+
+* HTML5
+* CSS3
+* Bootstrap 4.0
+* JavaScript
+* jQuery 3.3.1
+* jQuery UI 1.12.1
+
+
+##Issue - Fixed!
+When clicking on a card before the previous animation had finished, the program stopped running. Fixed by disabling the clicks until the previous animation has finished with a setTimeout() function. 
